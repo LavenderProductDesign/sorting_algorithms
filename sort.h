c064c9e5e2ba57d8be807617291c8886fa990b9a
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-07-19 23:52:29 +0300

    cocktail changes

--- sort.h
+++ sort.h
@@ -1,9 +1,25 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdlib.h>
-#include <stddef.h>
+
+/*includes*/
 #include <stdio.h>
-void swap(int *x, int *y);
+#include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define up 0
+#define down 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,56 +34,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-/**
- *enum kind_e - describes the kind
- * @SPADE: the card
- * @HEART: diff card
- * @CLUB: one card
- * @DIAMOND: check card
- */
-typedef enum kind_e
-{
-	SPADE = 0,
-	HEART,
-	CLUB,
-	DIAMOND
-} kind_t;
-
-/**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-	const char *value;
-	const kind_t kind;
-} card_t;
-
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
-
-/* Comparison direction macros for bitonic sort */
-#define up 0
-#define down 1
-
-void sortedInsert(listint_t **head_ref, listint_t *newNode);
-void print_array(const int *array, size_t size);
+/*Given printing functions*/
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/*Sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -80,8 +51,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-int copy_elements(int *arr1, int *arr2, size_t size);
-int r_largest(int *array, size_t size);
 
 #endif
+
